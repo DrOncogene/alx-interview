bec79c2321242a61136871fae4cab22a168aca7b
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-12-30 10:45:05 +0100

    more loop optimization

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,6 +8,12 @@ def pascal_triangle(n: int):
     """
     creates a list of lists that
     represents a Pascal's triangle
+
+    :param n: int
+        number of rows to compute
+
+    Returns: a list of list representing the
+             rows of the triangle
     """
     if n <= 0:
         return []
@@ -17,13 +23,11 @@ def pascal_triangle(n: int):
         return [[1], [1, 1]]
 
     triangle = [[1], [1, 1]]
-    for num in range(3, n + 1):
+    for _ in range(3, n + 1):
         last_row = triangle[-1]
         row = [1]
-        for i in range(1, num):
-            size = len(last_row)
-            if i < size:
-                row.append(last_row[i] + last_row[i - 1])
+        for i in range(1, len(last_row)):
+            row.append(last_row[i] + last_row[i - 1])
         row.append(1)
         triangle.append(row)
 
